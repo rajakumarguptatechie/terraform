f5b1ea41eb9fad871c80dec33f93b791ad3e6474
CentOS <centos@centos8.linuxvmimages.local>
2021-12-06 17:39:35 +0530

    terraform code to create instance on GCP

---
+++ terraform-training-vodafone/day-01/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/var/tmp/project-terraform-raj-d058c5d62d8a.json")
+
+  project = "project-terraform-raj"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["test", "training"]
+
+  boot_disk {
+    initialize_params {
+      #image = "debian-cloud/debian-9"
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
