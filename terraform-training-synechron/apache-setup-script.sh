1aabe7fa4fa717cbc761410b299dea3f7a75fb8a
rajakumarguptatechie <56320038+rajakumarguptatechie@users.noreply.github.com>
2022-03-15 11:41:42 +0530

    Add files via upload

---
+++ terraform-training-synechron/apache-setup-script.sh
@@ -0,0 +1,16 @@
+sudo yum install httpd -y	
+sudo service httpd start
+
+
+sudo cat << EOF > /var/tmp/index.html
+<!DOCTYPE html>
+<html>
+        <body>
+                <h1> Terraform Training </h1>
+                <p> This is to test Terraform provisioner by Raja Kumar Gupta </p>
+
+        </body>
+</html>
+EOF
+
+sudo cp /var/tmp/index.html /var/www/html/index.html
